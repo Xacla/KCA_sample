9f4b2798af0474e2ecb56791d368a5a9de7de548
Tom174 <area184persona@gmail.com>
2017-08-29 17:12:30 +0900

    カーネル行列の生成の追加

--- elipsoid_kca.py
+++ elipsoid_kca.py
@@ -9,10 +9,9 @@ def create_elipsoid(a,b,c,theta,phi):
     y=b*np.sin(theta)*np.sin(phi)
     z=c*np.cos(theta)
     return x,y,z
-
-def create_swisroll():
-    for i in  [0.01*i for in range(200)]:
-        if
+def kernel_gause(x_i,x_j):
+    x_norm=np.linalg.norm(x_j-x_i)
+    return np.exp(x_norm)
 
 a=1
 b=1
@@ -22,10 +21,12 @@ circle_x=[]
 circle_y=[]
 hight=[]
 data=[]
+RONDAM_NUM=1500
 
+#楕円型のデータを作成
 for theta in [0.01*i for i in range(314)]:
     for phi in [0.01*k for k in range(314*2)]:
-        if random.randint(0,314)==1:
+        if random.randint(0,RONDAM_NUM)==1:
             x,y,z=create_elipsoid(a,b,c,theta,phi)
             box=[x,y,z]
             circle_x.append(x)
@@ -43,10 +44,37 @@ ax.scatter(circle_x,circle_y,hight)
 plt.show()
 '''
 
+#PCAの場合
 data=np.array(data)
 pca=PCA()
 pca.fit(data)
 pca_data=pca.fit_transform(data)
-
 plt.scatter(pca_data[:,0],pca_data[:,1])
 plt.show()
+
+#KCAの場合
+kernel_gause(data[20,:],data[1,:])
+
+#カ-ネル行列の生成
+kernel_matrix=[]
+ones_matrix=[]
+data=np.array(data)
+for i in range(data.shape[0]):
+    a=[]
+    ones_row=[]
+    for j in range(data.shape[0]):
+        result_gause=kernel_gause(data[i,:],data[j,:])
+        a.append(result_gause)
+        ones_row.append(1)
+    kernel_matrix.append(a)
+    ones_matrix.append(ones_row)
+
+kernel_matrix=np.array(kernel_matrix)
+ones_matrix=np.array(ones_matrix)
+n_reverse=1/kernel_matrix.shape[0]
+ones_matrix=n_reverse*ones_matrix
+print(kernel_matrix.shape)
+print(ones_matrix[0,0])
+
+i_n=np.identity(kernel_matrix.shape[0])
+j_n=i_n - ones_matrix
